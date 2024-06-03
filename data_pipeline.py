366f18f5fc08e9380f8fad1a9ecbcdffb8f590a6
Javiera Parada <jparada@mma.gob.cl>
2024-06-03 17:06:48 -0400

    cambios en file output

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,6 +1,8 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import csv
+import logging
+
 
 class FormatData(beam.DoFn):
     def process(self, element):
@@ -10,13 +12,16 @@ class FormatData(beam.DoFn):
                 element['VehicleMake'] = element.pop('Make')
                 element['VehicleModel'] = element.pop('Model')
                 element['EVType'] = element.pop('Electric Vehicle Type')
-                # Creando nuevas variables
+
+                # Crea nuevas variables
                 element['IsEligible'] = 1 if element.pop('Clean Alternative Fuel Vehicle (CAFV) Eligibility') == 'Clean Alternative Fuel Vehicle Eligible' else 0
+
                 yield element
             except KeyError as e:
-                print(f"Error procesando el elemento: {element}. Error: {e}")
-        else:
-            print("Elemento vacío encontrado")
+                logging.error(f"Error procesando el elemento: {element}. Error: {e}")
+            else:
+                logging.info("Elemento vacío encontrado")
+
 
 def run():
     options = PipelineOptions()
@@ -32,13 +37,14 @@ def run():
     headers = ['VIN (1-10)', 'County', 'City', 'State', 'Postal Code', 'Model Year', 'Make', 'Model', 'Electric Vehicle Type', 'Clean Alternative Fuel Vehicle (CAFV) Eligibility', 'Electric Range', 'Base MSRP', 'Legislative District', 'DOL Vehicle ID', 'Vehicle Location', 'Electric Utility', '2020 Census Tract']
 
     # Leer y procesar datos del CSV
-    (p
-     | 'ReadCSV' >> beam.io.ReadFromText(input_file, skip_header_lines=1)
-     | 'ParseCSV' >> beam.Map(lambda line: dict(zip(headers, next(csv.reader([line])))) if line else None)
-     | 'PrintParsedData' >> beam.Map(print)  # Imprimir datos parseados
-     | 'TransformData' >> beam.ParDo(FormatData())
-     | 'PrintTransformedData' >> beam.Map(print)  # Imprimir datos transformados
-     | 'WriteOutput' >> beam.io.WriteToText(r'C:/Users/javie/Desktop/D-E/YOM/mvp_de/mnt/data/output/transformed_data', file_name_suffix='.csv', shard_name_template=''))
+    (
+        p
+        | "ReadCSV" >> beam.io.ReadFromText(input_file, skip_header_lines=1)
+        | "ParseCSV" >> beam.Map(lambda line: dict(zip(headers, next(csv.reader([line])))) if line else None)
+        | "FilterEmptyLines" >> beam.Map(lambda element: element if element else beam.Row())  # Filtrar filas vacías
+        | "FormatData" >> beam.ParDo(FormatData())
+        | "WriteOutput" >> beam.io.WriteToText(r'C:/Users/javie/Desktop/D-E/YOM/mvp_de/mnt/data/output/transformed_data', file_name_suffix='.csv', shard_name_template='')
+    )
 
     p.run().wait_until_finish()
     """ 
@@ -57,5 +63,6 @@ def upload_to_gcs(output_path):
     blob = bucket.blob(f"output/transformed_data_{current_date}.csv")
     blob.upload_from_filename(output_path + '-00000-of-00001') """
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
